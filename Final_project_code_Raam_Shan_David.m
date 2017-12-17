97add3e13df94f7d9831bba1981606da98e7ca38
dsalmi2 <34633190+dsalmi2@users.noreply.github.com>
2017-12-17 16:17:27 -0600

    Add files via upload

---
+++ Final_project_code_Raam_Shan_David.m
@@ -0,0 +1,38 @@
+%%constants
+initialxvector = 
+initialyvector = 
+initialzvector = 
+
+%given to us from Guto
+finalxvector = 
+finalyvector = 
+finalzvector = 
+
+%constants for 3U satellite
+xmoi = 
+ymoi = 
+zmoi =
+
+%given to us from Guto
+t = 
+
+%constants for torque strength of reaction wheels
+torque = 
+
+%friction from reation wheels 
+%(most likely equation in terms of rotational velocity of wheel)
+friction =
+
+%
+%% steps needed to solve the problem and simulate the result
+
+%create three dimensional model of the satellite
+
+%create vectors from final to initial positions
+finalpos = [finalxvector;finalyvector;finalzvector]
+initialpos = [initialxvector;initialyvector;initialzvector]
+
+%
+
+
+
